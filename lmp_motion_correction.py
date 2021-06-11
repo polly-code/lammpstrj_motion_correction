8f58c823a723bc5d0b26481c29ef18bc0f83e14d
polly-code <52492217+polly-code@users.noreply.github.com>
2021-06-11 13:27:02 +0200

    script uploaded

---
+++ lmp_motion_correction.py
@@ -0,0 +1,9 @@
+import scipy
+import mdtraj as md
+import sys
+tr=md.load(sys.argv[1], top=sys.argv[2])
+tr.center_coordinates()
+for i in range(1,len(tr)):
+    f1, _ = scipy.spatial.transform.Rotation.align_vectors(tr.xyz[i-1],tr.xyz[i])
+    tr.xyz[i] = f1.apply(tr.xyz[i])
+tr.save_lammpstrj(sys.argv[1].replace('.lammpstrj','_c.lammpstrj'))
